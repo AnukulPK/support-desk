512f0fb46822a10fc3dd07808df41d3e09c10df1
AnukulPK <anukulpk2@gmail.com>
2022-12-13 11:04:07 +0530

    notes backend

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -9,6 +9,10 @@ const {
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 
+// Re-route into note router
+const noteRouter = require("./noteRoutes");
+router.use("/:ticketId/notes", noteRouter);
+
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 router
   .route("/:id")
